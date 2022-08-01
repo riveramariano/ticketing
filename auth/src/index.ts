1c48578fb8dbda49ba067c6fcb1b7eb2bc57ec84
Mariano Rivera <rivveramariano@gmail.com>
2022-07-31 20:04:46 -0600

    Added JWT
    Added JWT and it's secret to sign-up route.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signInRouter } from './routes/sign-in';
@@ -11,7 +12,17 @@ import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+
+app.set('trust proxy', true);
 app.use(json());
+
+// This is a middleware that is used to store the JWT in the cookie
+app.use(
+  cookieSession({
+    signed: false, // Disable Encryption
+    secure: true, // Require HTTPS
+  })
+);
 
 // Define the auth micro-service routes
 app.use(currentUserRouter);
@@ -27,6 +38,10 @@ app.all('*', async () => {
 app.use(errorHandler);
 
 const startUp = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
     console.log('Auth service connected to MongoDB!');
--- auth/src/routes/sign-up.ts
+++ auth/src/routes/sign-up.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator'
+import jwt from 'jsonwebtoken';
+
 import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
@@ -33,6 +35,15 @@ router.post('/api/users/sign-up',
 
     const user = User.build({ email, password });
     await user.save();
+
+    // Generate JWT
+    const userJwt = jwt.sign({
+      id: user.id,
+      email: user.email
+    }, process.env.JWT_KEY!);
+
+    // Store it on session object
+    req.session = { jtw: userJwt };
 
     res.status(201).send(user);
   }
