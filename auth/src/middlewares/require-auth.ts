68e08c2b956b50aea72cab874a70a82994482b86
Mariano Rivera <rivveramariano@gmail.com>
2022-08-01 14:17:38 -0600

    Not Authorized Middleware
    Checks if the user is authorized or not.

--- auth/src/middlewares/require-auth.ts
+++ auth/src/middlewares/require-auth.ts
@@ -1,11 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
+import { NotAuthorizedError } from '../errors/not-authorized-error';
 
 export const requireAuth = (
   req: Request,
-  res: Response,
+  _res: Response,
   next: NextFunction
 ) => {
   if (!req.currentUser) {
-    return res.status(401).send();
+    throw new NotAuthorizedError();
   }
+
+  next();
 };
--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,14 +1,10 @@
 import express from 'express';
-
 import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/current-user', 
-  currentUser,
-  (req, res) => {
-    res.send({ currentUser: req.currentUser || null });
-  }
-);
+router.get('/api/users/current-user', currentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser || null });
+});
 
 export { router as currentUserRouter };
