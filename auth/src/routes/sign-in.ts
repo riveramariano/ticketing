25c503212a7c7792bf7c0062d838a5420629026e
Mariano Rivera <rivveramariano@gmail.com>
2022-08-01 13:32:36 -0600

    Added Sign In Functionality
    Now a user can sign in into the application.

--- auth/src/routes/sign-in.ts
+++ auth/src/routes/sign-in.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
 
 import { validateRequest } from '../middlewares/validate-request';
-import { RequestValidationError } from '../errors/request-validation-error';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
+import { Password } from '../services/password';
 
 const router = express.Router();
 
@@ -18,8 +21,29 @@ router.post('/api/users/sign-in',
       .withMessage('You must supply a password')
   ],
   validateRequest,
-  (req: Request, res: Response) => {
-    res.send("Hi there!");
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    const passwordsMatch = await Password.compare(existingUser.password, password);
+    if (!passwordsMatch) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    // Generate JWT
+    const userJwt = jwt.sign({
+      id: existingUser.id,
+      email: existingUser.email
+    }, process.env.JWT_KEY!);
+
+    // Store it on session object
+    req.session = { jtw: userJwt };
+
+    res.status(201).send(existingUser);
   }
 );
 
--- auth/src/routes/sign-up.ts
+++ auth/src/routes/sign-up.ts
@@ -1,10 +1,9 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator'
+import { body } from 'express-validator'
 import jwt from 'jsonwebtoken';
 
 import { validateRequest } from '../middlewares/validate-request';
 import { User } from '../models/user';
-import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
