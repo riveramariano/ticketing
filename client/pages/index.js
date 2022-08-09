940f20ccda498eb6efd01228fda479151ba5870b
Mariano Rivera <rivveramariano@gmail.com>
2022-08-09 10:00:57 -0600

    Big SSR Problem
    It will be solved in the next commit.

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,18 @@
-const index = () => {
-  return <div>Hello2 </div>;
-}
+import axios from 'axios';
 
-export default index;
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+
+  return <h1>LandingPage</h1>;
+};
+
+// Executed during the SSR process
+LandingPage.getInitialProps = async () => {
+  const response = axios.get('/api/users/current-user').catch((err) => {
+    console.log(err.message);
+  }); 
+
+  return response.data;
+};
+
+export default LandingPage;
