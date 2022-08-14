484f14a26cd6e03b3e352b8518db87e5e40f665b
Mariano Rivera <rivveramariano@gmail.com>
2022-08-13 20:32:19 -0600

    Fetch CurrentUser From App

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <h1>{currentUser.email}</h1>
+      <Component {...pageProps} />
+    </div>
+  )
 };
+
+// Executed during the SSR process
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/current-user');
+
+  // Ability to access children components initialProps
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return { pageProps, ...data }
+};
+
+export default AppComponent;
