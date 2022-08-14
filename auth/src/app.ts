a19165fa28ae0a49d84161e0f1301c014521a3f3
Mariano Rivera <rivveramariano@gmail.com>
2022-08-14 14:59:01 -0600

    Fixed Auth Errors

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -2,13 +2,12 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError } from '@mrtickers/common';
 
 import { currentUserRouter } from './routes/current-user';
 import { signInRouter } from './routes/sign-in';
 import { signOutRouter } from './routes/sign-out';
 import { signUpRouter } from './routes/sign-up';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 
