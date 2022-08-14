79c955909e6951cc852208c2bb802141593def93
Mariano Rivera <rivveramariano@gmail.com>
2022-08-13 19:59:56 -0600

    Fix SSR Requests

--- client/pages/index.js
+++ client/pages/index.js
@@ -7,12 +7,32 @@ const LandingPage = ({ currentUser }) => {
 };
 
 // Executed during the SSR process
-LandingPage.getInitialProps = async () => {
-  const response = axios.get('/api/users/current-user').catch((err) => {
-    console.log(err.message);
-  }); 
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    /*
+      We're on the server when:
+        - Hard refresh of page
+        - Clicking link from different domain
+        - Typing URL into address bar
+    */
+    const { data } = await axios.get(
+      // http://SERVICENAME.NAMESPACE.svc.cluster.local/ROUTE
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/current-user', {
+        headers: req.headers
+      }
+    );
 
-  return response.data;
+    return data;
+  } else {
+    /*
+      We're on the browser when:
+        - Navigating from one page to another while in the app
+          - From sign-up to landing page
+    */
+    const { data } = await axios.get('/api/users/current-user');
+
+    return data;
+  }
 };
 
 export default LandingPage;
