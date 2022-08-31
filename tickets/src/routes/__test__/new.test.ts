95eec4ff6f1242a43db651061fc02ea03b8b3800
Mariano Rivera <rivveramariano@gmail.com>
2022-08-31 15:47:43 -0600

    Finished Unit Testing

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
   const response = await request(app)
@@ -54,11 +55,15 @@ it('returns an error if an invalid price is provided', async () => {
 });
 
 it('creates a ticket with valid inputs', async () => {
-  // Add in a check to make sure a ticket was saved
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
 
   await request(app)
     .post('/api/tickets')
     .set('Cookie', global.getCookie())
     .send({ title: 'Ticket', price: 10 })
     .expect(201);
+
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
 });
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@mrtickers/common';
+import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
@@ -11,8 +12,17 @@ router.post('/api/tickets',
   ],
   validateRequest,
   requireAuth,
-  (req: Request, res: Response) => {
-    res.sendStatus(200);
+  async (req: Request, res: Response) => {
+    const { title, price } = req.body;
+
+    const ticket = Ticket.build({
+      title,
+      price,
+      userId: req.currentUser!.id
+    });
+    await ticket.save();
+
+    res.status(201).send(ticket);
   }
 );
 
