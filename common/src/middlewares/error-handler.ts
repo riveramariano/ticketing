b8aba08b0c5dbabd8c5f3dd389becefb2f4d4168
Mariano Rivera <rivveramariano@gmail.com>
2022-09-04 17:26:09 -0600

    Updates

--- common/src/middlewares/error-handler.ts
+++ common/src/middlewares/error-handler.ts
@@ -10,6 +10,7 @@ export const errorHandler = (
   if (err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   } else {
+    console.error(err);
     res.status(400).send({ errors: [{ message: "Something went wrong" }] });
   }
 };
