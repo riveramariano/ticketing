37afa2b59340491ad4f0e3fa03a015d500921052
Mariano Rivera <rivveramariano@gmail.com>
2022-09-04 17:35:40 -0600

    Update Common Folder

--- tickets/src/routes/show.ts
+++ tickets/src/routes/show.ts
@@ -1,12 +1,16 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
-import { requireAuth, validateRequest } from '@mrtickers/common';
+import { requireAuth, validateRequest, NotFoundError } from '@mrtickers/common';
 import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
 router.get('/api/tickets/:id', async (req: Request, res: Response) => {
+  const ticket = await Ticket.findById(req.params.id);
 
+  if (!ticket) { throw new NotFoundError(); }
+
+  res.send(ticket);
 });
 
 export { router as showTicketRouter };
