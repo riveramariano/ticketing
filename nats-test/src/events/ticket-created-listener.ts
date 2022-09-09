b7b35494bb7d5cbd62cd5ae91b9797b22c7565cf
Mariano Rivera <rivveramariano@gmail.com>
2022-09-09 11:48:49 -0600

    TS Validations

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,12 +1,17 @@
 import { Message } from 'node-nats-streaming';
 import { Listener } from "./base-listener";
+import { TicketCreatedEvent } from './ticket-created-event';
+import { Subjects } from './subjects';
 
-export class TicketCreatedListener extends Listener {
-  subject = 'ticket:created';
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  readonly subject = Subjects.TicketCreated;
   queueGroupName = 'payments-service';
 
-  onMessage(data: any, msg: Message): void {
+  onMessage(data: TicketCreatedEvent['data'], msg: Message): void {
     console.log('Event data!', data);
+    console.log(data.id);
+    console.log(data.title);
+    console.log(data.price);
 
     msg.ack();
   }
