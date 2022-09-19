cace4710847643fbdbf702f77edd5add119c8fbb
Mariano Rivera <rivveramariano@gmail.com>
2022-09-18 22:15:04 -0600

    Added ENV Variables

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { validateRequest, requireAuth } from '@mrtickers/common';
 import { Ticket } from '../models/ticket';
-import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+import { TicketCreatedPublisher } from '../events/ticket-created-publisher';
 import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
