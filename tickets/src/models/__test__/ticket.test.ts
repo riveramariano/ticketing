0d12af3df25b635d5d921f7cb5087bfc74f9ec06
Mariano Rivera <rivveramariano@gmail.com>
2022-10-18 07:50:53 -0600

    Concurrency Unit Testing

---
+++ tickets/src/models/__test__/ticket.test.ts
@@ -0,0 +1,50 @@
+import { Ticket } from '../ticket';
+
+it('implements optimistic concurrency control', async () => {
+  // Create an instance of a ticket
+  const ticket = Ticket.build({
+    title: 'Concert',
+    price: 20,
+    userId: '1234'
+  });
+
+  // Save the ticket to the database
+  await ticket.save();
+
+  // Fetch the ticket twice
+  const firstInstance = await Ticket.findById(ticket.id);
+  const secondInstance = await Ticket.findById(ticket.id);
+
+  // Make two separate changes to the ticket we fetched
+  firstInstance!.set({ price: 50 });
+  secondInstance!.set({ price: 80 });
+
+  // Save the first fetched ticket
+  await firstInstance!.save();
+
+  // Save the second fetched ticket and expect an error
+  try {
+    await secondInstance!.save();
+  } catch (err) {
+    return;
+  }
+
+  throw new Error('Should not reach this point');
+});
+
+it('increments the version number on multiple saves', async () => {
+  const ticket = Ticket.build({
+    title: 'Concert',
+    price: 20,
+    userId: '1234'
+  });
+
+  await ticket.save();
+  expect(ticket.version).toEqual(0);
+
+  await ticket.save();
+  expect(ticket.version).toEqual(1);
+
+  await ticket.save();
+  expect(ticket.version).toEqual(2);
+});
