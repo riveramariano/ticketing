6cd673f3a915060d2657208d0ce5de633e6051ae
Mariano Rivera <rivveramariano@gmail.com>
2022-10-26 15:16:28 -0600

    Logic Improvements

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -6,7 +6,7 @@ const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <div>
       <Header currentUser={currentUser} />
-      <Component {...pageProps} />
+      <Component currentUser={currentUser} {...pageProps} />
     </div>
   )
 };
@@ -19,7 +19,7 @@ AppComponent.getInitialProps = async (appContext) => {
   // Ability to access children components initialProps
   let pageProps = {};
   if (appContext.Component.getInitialProps) {
-    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx, client, data.currentUser);
   }
 
   return { pageProps, ...data }
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,15 +1,10 @@
-import buildClient from "../api/build-client"; 
-
 const LandingPage = ({ currentUser }) => {
   return currentUser ? <h1>You're signed in</h1> : <h1>You're not signed-in</h1>
 };
 
 // Executed during the SSR process
-LandingPage.getInitialProps = async (context) => {
-  const client = buildClient(context);
-  const { data } = await client.get('/api/users/current-user');
-
-  return data;
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  return {};
 };
 
 export default LandingPage;
