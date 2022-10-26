8719cef2788d60f150630fbd3d38c41474ef9375
Mariano Rivera <rivveramariano@gmail.com>
2022-10-26 16:57:26 -0600

    Fixed Issues

--- client/components/header.js
+++ client/components/header.js
@@ -4,6 +4,8 @@ export default ({ currentUser }) => {
   const links = [
     !currentUser && { label: 'Sign Up', href: '/auth/sign-up' },
     !currentUser && { label: 'Sign In', href: '/auth/sign-in' },
+    currentUser && { label: 'Sell Tickets', href: '/tickets/new' },
+    currentUser && { label: 'My Orders', href: '/orders' },
     currentUser && { label: 'Sign Out', href: '/auth/sign-out' }
   ]
     .filter((linkConfig) => linkConfig)
