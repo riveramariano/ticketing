23ad2cc47e5b2ff5d31e3c6c5f89b08be351a19e
Mariano Rivera <rivveramariano@gmail.com>
2022-10-27 09:41:41 -0600

    Clearer Console Logs

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -8,19 +8,19 @@ import { PaymentCreatedListener } from './events/listeners/payment-created-liste
 
 const startUp = async () => {
   if (!process.env.JWT_KEY) {
-    throw new Error('JWT_KEY must be defined');
+    throw new Error('Orders JWT_KEY must be defined');
   }
   if (!process.env.MONGO_URI) {
-    throw new Error('MONGO_URI must be defined');
+    throw new Error('Orders MONGO_URI must be defined');
   }
   if (!process.env.NATS_CLUSTER_ID) {
-    throw new Error('NATS_CLUSTER_ID must be defined');
+    throw new Error('Orders NATS_CLUSTER_ID must be defined');
   }
   if (!process.env.NATS_CLIENT_ID) {
-    throw new Error('NATS_CLIENT_ID must be defined');
+    throw new Error('Orders NATS_CLIENT_ID must be defined');
   }
   if (!process.env.NATS_URL) {
-    throw new Error('NATS_URL must be defined');
+    throw new Error('Orders NATS_URL must be defined');
   }
 
   try {
@@ -30,7 +30,7 @@ const startUp = async () => {
       process.env.NATS_URL
     );
     natsWrapper.client.on('close', () => {
-      console.log('NATS connection closed!');
+      console.log('Orders NATS connection closed!');
       process.exit();
     });
     process.on('SIGINT', () => natsWrapper.client.close());
@@ -42,13 +42,13 @@ const startUp = async () => {
     new PaymentCreatedListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('Orders service connected to MongoDB!');
+    console.log('Orders connected to MongoDB!');
   } catch (err) {
     console.log(err);
   }
 
   app.listen(3000, () => {
-    console.log('Orders service on port 3000!');
+    console.log('Orders on port 3000!');
   });
 };
 
